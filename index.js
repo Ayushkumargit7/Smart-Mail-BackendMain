97f8235bb8fac15e7026ee09a49e2c28c23c0e09
Ayush Kumar <114018461+Ayushkumargit7@users.noreply.github.com>
2024-07-18 00:08:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.post('/generate', async (req, res) => {
     }
   
     // For text-only input, use the gemini-pro model
-    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
   
     const result = await model.generateContent(prompt);
     const response = await result.response;
