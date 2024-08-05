8d5fda588710430860cd41a27bfd89c4a863cfc9
Ayush Kumar <114018461+Ayushkumargit7@users.noreply.github.com>
2024-08-06 00:34:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ import cors from 'cors';
 import Connection from './database/db.js';
 import routes from './routes/route.js';
 import Email from './model/email.js';
-
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import nodemailer from "nodemailer";
+import { initializeRedisClient, redisCachingMiddleware } from './middlewares/redis.js';
 import dotenv from "dotenv";
 dotenv.config();
-
-import nodemailer from "nodemailer";
-
 
 const app = express();
 
@@ -18,8 +16,15 @@ app.use(express.urlencoded());
 app.use(express.json());
 app.use('/', routes);
 
-// const PORT = 8000;
-const port = process.env.PORT || 3000;
+const PORT = 8000;
+
+
+// Initialize Redis client
+initializeRedisClient();
+
+// Use the middleware in your routes
+app.use('/api', redisCachingMiddleware({ EX: 600 })); // Cache for 600 seconds.
+
 
 app.get('/', (req, res) => {
   res.send(`Server is running`);
@@ -46,22 +51,21 @@ const sendMail = async (mailOptions) => {
   }   
 }
 
-// Update email type endpoint
-app.post('/api/update-email', async (req, res) => {
-  const { id, type2 } = req.body;
+// // Update email type endpoint
+// app.post('/api/update-email', async (req, res) => {
+//   const { id, type2 } = req.body;
 
-  try {
-      const email = await Email.updateOne({_id:id }, {$set: { type : "read" }});
-      console.log(email);
-      if (!email) return res.status(404).send('The email with the given ID was not found.');
-      res.send(email);
-  } catch (error) {
-      res.status(500).send('Something went wrong.');
-  }
-});
+//   try {
+//       const email = await Email.updateOne({_id:id }, {$set: { type : "read" }});
+//       console.log(email);
+//       if (!email) return res.status(404).send('The email with the given ID was not found.');
+//       res.send(email);
+//   } catch (error) {
+//       res.status(500).send('Something went wrong.');
+//   }
+// });
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-
 
 app.post('/generate', async (req, res) => {
     const prompt = req.body.prompt;
@@ -76,7 +80,6 @@ app.post('/generate', async (req, res) => {
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
-    // console.log(text);
     res.send({ generatedText: text });
   });
 
@@ -103,5 +106,4 @@ app.post('/generate', async (req, res) => {
 
 Connection();
 
-// app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
-app.listen(port, "0.0.0.0",() => console.log(`Server started on PORT ${port}`));
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
