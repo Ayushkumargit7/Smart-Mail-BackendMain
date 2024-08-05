35516ea2c318923bdb0872583fbbcebd0e79fe2b
Ayush Kumar <114018461+Ayushkumargit7@users.noreply.github.com>
2024-08-06 00:34:05 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -3,11 +3,14 @@ import express from 'express';
 import { saveSendEmails, getEmails, toggleStarredEmail, deleteEmails, 
     moveEmailsToBin } from '../controller/email-controller.js';
 
+import { redisCachingMiddleware } from '../middlewares/redis.js';
+
 const routes = express.Router();
+
+routes.get('/emails/:type', redisCachingMiddleware({ EX: 600 }), getEmails);
 
 routes.post('/save', saveSendEmails);
 routes.post('/save-draft', saveSendEmails);
-routes.get('/emails/:type', getEmails);
 routes.post('/starred', toggleStarredEmail);
 routes.delete('/delete', deleteEmails);
 routes.post('/bin', moveEmailsToBin);
